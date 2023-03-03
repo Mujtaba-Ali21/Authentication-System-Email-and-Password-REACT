1ae6ccc6536f6752179496b02cb9362a0b18ff2d
Mujtaba-Ali21 <mujtabaaliprince730@gmail.com>
2023-03-03 13:13:25 +0500

    fixed anchor tag issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand">Navbar</a>
+                <i className="navbar-brand">Navbar</i>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
